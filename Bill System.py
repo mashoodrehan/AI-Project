6a1dd52d5ad927dd88ccf5ed69e838bf31b92907
mashoodrehan <mashoodrehan5@gmail.com>
2021-01-10 19:44:26 +0500

    Frames Added

--- Bill System.py
+++ Bill System.py
@@ -12,3 +12,20 @@ root.configure(background = '#312113')
 text_Input = StringVar()
 operator = ""
 
+Tops = Frame(root,bg='#312113',bd=5,pady=5,relief=GROOVE)
+Tops.pack(side=TOP)
+lblTitle = Label(Tops,font=('times',30,'bold italic'),text="Bahria Bakery",bd=18,bg='#312113',fg='#FFD700',justify=CENTER)
+lblTitle.grid(row=0,column=0)
+
+Copyright = Label(root ,bg = '#312113',fg='#FFD700', width = 1200, font=('times', 10, 'bold italic') , text = '© 2020 Copyright. All Rights Reserved')
+Copyright.pack(side = BOTTOM)
+
+ReceiptCal_F = Frame(root,bg='#312113',bd=5,padx=20,relief=FLAT)
+ReceiptCal_F.pack(side=RIGHT)
+
+Buttons_F=Frame(ReceiptCal_F,bg='#312113',bd=4,relief=RIDGE)
+Buttons_F.pack(side=BOTTOM)
+
+Cal_F=Frame(ReceiptCal_F,bg='#312113',bd=6,relief=RIDGE)
+Cal_F.pack(side=BOTTOM)
+
