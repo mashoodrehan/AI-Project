b48c9dc0856c4525211ea9b3433db568050e39d6
mashoodrehan <mashoodrehan5@gmail.com>
2021-01-10 19:43:42 +0500

    Libraries added

---
+++ Bill System.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random
+import datetime
+import time
+import tkinter.messagebox
+
+root = Tk()
+root.geometry("1300x750+0+0")
+root.title("Bahria Bakery Billing System")
+root.configure(background = '#312113')
+
+text_Input = StringVar()
+operator = ""
+
